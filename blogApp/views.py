6f7da406c03068fb6de9bee935fd9c395ffb7a0a
David Fitzgerald <davidjmfitzgerald@gmail.com>
2019-06-22 17:56:05 -0400

    lesson 3

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts =[
+	{
+		'author': 'David Fitzgerald',
+		'title': 'Post',
+		'content': 'First post content',
+		'date_posted': 'June 22, 2019'
+	},
+	{
+		'author': 'Madelaine Feakins',
+		'title': 'Post 2',
+		'content': 'Second post content',
+		'date_posted': 'June 23, 2019'
+	}
+]
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blogApp/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About:</h1>')
+	return render(request, 'blogApp/about.html', {'title': 'About'})
